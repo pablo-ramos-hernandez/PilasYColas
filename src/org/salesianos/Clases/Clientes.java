a57c2b93cc292026554191a619f27ee95f473378
pablo-ramos-hernandez <126242940+pablo-ramos-hernandez@users.noreply.github.com>
2023-05-03 22:50:17 +0100

    fix(Clientes):Correccion de la clase CestaDeLaCompra y Cliente

--- src/org/salesianos/Clases/Clientes.java
+++ src/org/salesianos/Clases/Clientes.java
@@ -9,20 +9,5 @@ public class Clientes {
 
     // Creacion de la lista de productos
 
-    private static final String[] CLIENTE_NAMES = new String[] {
-
-        "Amanda", "Sofía", "Tito", "Ana", "German", "Juan", "Pablo", "Rossi", "Valentin", "Ioan", 
-        "German", "Rita", "Juani", "Jose", "Paola", "Josué", "Ricardo", "Alejandro", "Jordi", "Carlos"
-
-    };
-
-    // Creacion de la utilidad de obtener un producto aleatorio
-    public static String getRandomItems() {
-        Random random = new Random();
-
-        int randomIndex = random.nextInt(Clientes.CLIENTE_NAMES.length);
-
-        return Clientes.CLIENTE_NAMES[randomIndex];
-    };
 
 }
