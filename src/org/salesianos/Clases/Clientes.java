6e836509346aa8dc930d90d471b875b074f71572
pablo-ramos-hernandez <126242940+pablo-ramos-hernandez@users.noreply.github.com>
2023-05-03 22:40:33 +0100

    fix(Clientes):Arreglado array de los cliente

--- src/org/salesianos/Clases/Clientes.java
+++ src/org/salesianos/Clases/Clientes.java
@@ -11,13 +11,13 @@ public class Clientes {
 
     private static final String[] CLIENTE_NAMES = new String[] {
 
-        "German"
+        "Amanda", "Sofía", "Tito", "Ana", "German", "Juan", "Pablo", "Rossi", "Valentin", "Ioan", 
+        "German", "Rita", "Juani", "Jose", "Paola", "Josué", "Ricardo", "Alejandro", "Jordi", "Carlos"
 
     };
 
     // Creacion de la utilidad de obtener un producto aleatorio
     public static String getRandomItems() {
-
         Random random = new Random();
 
         int randomIndex = random.nextInt(Clientes.CLIENTE_NAMES.length);
